f2409439b6adc75182704e31a0cf9024d6d14b26
Nithya <jaynithya123@gmail.com>
2020-12-17 10:11:33 -0500

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+#Basic Recognition
+A very basic Python script for facial recognition without any Machine Learning elements.
---
+++ trackingImage.py
@@ -0,0 +1,17 @@
+import cv2
+import sys
+
+image = sys.argv[1]
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+img = cv2.imread(image)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 4, minSize=(30, 30))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Faces", img)
+    cv2.waitKey(0)
+
