af2342abd80d5f960baa63c1b69b20e142768499
Nithya <jaynithya123@gmail.com>
2020-12-17 15:08:16 -0500

    clean up debugging print statements

--- faces-train.py
+++ faces-train.py
@@ -23,19 +23,15 @@ for root, dirs, files in os.walk(image_dir):
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
-            #print(label, path)
             if not label in label_ids: 
                 label_ids[label] = current_id
                 current_id += 1
             id_ = label_ids[label]
-            #print(label_ids)
-            #y_labels.append(label)
-            #x_train.append(path)
+            
             pil_image = Image.open(path).convert("L")
             size = (550, 550)
             final_image = pil_image.resize(size, Image.ANTIALIAS)
             image_array = np.array(final_image, "uint8")
-            #print(image_array)
 
             faces = faceCascade.detectMultiScale(image_array, scaleFactor = 1.1, minNeighbors = 4)
 
@@ -43,8 +39,7 @@ for root, dirs, files in os.walk(image_dir):
                 roi = image_array[y:y + h, x:x + w]
                 x_train.append(roi)
                 y_labels.append(id_)
-#print(y_labels)
-#print(x_train)
+                
 with open("labels.pickle", 'wb') as f:
     pickle.dump(label_ids, f)
 
